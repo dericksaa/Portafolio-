b00f42668032d91380f91e04a48b10ca5a29d2d5
dericksaa <dmsaao@unal.edu.co>
2023-01-13 20:06:54 -0500

    menu final

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -11,18 +11,20 @@ const Header = () => {
   return (
     <header className='header'>
         <div className='logoso'></div>
-        <button className='menuButtom' onClick={camb}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" fill="white" viewBox="0 0 16 16">
-          <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-        </svg>
-        </button>
-        <nav className={`nav ${menu?'isActive':''}`}>
-          <a href="">About</a>
-          <a href="">Training</a>
-          <a href="">Skills</a>
-          <a href="">Projects</a>
-          <a href="">Contact</a>
-        </nav>
+        <div className='hamburguesa'>
+          <button className='menuButtom' onClick={camb}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" fill="#E7A21E" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+          </svg>
+          </button>
+          <nav className={`nav ${menu?'isActive':''}`}>
+            <a href="">About</a>
+            <a href="">Training</a>
+            <a href="">Skills</a>
+            <a href="">Projects</a>
+            <a href="">Contact</a>
+          </nav>
+        </div>
     </header>
   )
 }
