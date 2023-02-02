bc579f572465c87870a825096ea5aef44044f438
dericksaa <dmsaao@unal.edu.co>
2023-02-02 15:18:55 -0500

    change order training

--- src/Components/Training.jsx
+++ src/Components/Training.jsx
@@ -7,13 +7,13 @@ const Training = () => {
     <div className='training' id='training'>
     <h2>training</h2>
     <div className='cardsTraining'>
-        <div className='trainingCont trainCont1'>
-            <h3>UNIVERSIDAD NACIONAL DE COLOMBIA</h3>
-            <h3>mechanical engineer</h3>
-        </div>
         <div className='trainingCont trainCont2'>
             <h3>EDUCAMAS</h3>
             <h3>full stack web developer</h3>
+        </div>
+        <div className='trainingCont trainCont1'>
+            <h3>UNIVERSIDAD NACIONAL DE COLOMBIA</h3>
+            <h3>mechanical engineer</h3>
         </div>
     </div>
     </div>
